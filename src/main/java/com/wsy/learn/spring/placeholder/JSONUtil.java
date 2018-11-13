32ffcba2b3601634b7923ceee2ee531a50374f25
wangsiyuan6 <wangsiyuan1@jd.com>
2018-11-13 10:49:38 +0800

    支持目录迭代json文件

--- src/main/java/com/wsy/learn/spring/placeholder/JSONUtil.java
+++ src/main/java/com/wsy/learn/spring/placeholder/JSONUtil.java
@@ -13,29 +13,33 @@ public class JSONUtil {
 
     File in;
 
-    public File getIn() {
-        return in;
+    public void getConfigAsMap() {
+        try {
+            if(in == null || !in.isDirectory()) {
+                throw new RuntimeException("template is not a dir");
+            }
+            File[] files = in.listFiles();
+            if(files == null) {
+                throw new RuntimeException("dir file list is empty");
+            }
+            for (File file : files) {
+                FileReader reader = new FileReader(file);
+                BufferedReader br = new BufferedReader(reader);
+
+                StringBuilder builder = new StringBuilder();
+                String line = "";
+                while ((line = br.readLine()) != null) {
+                    builder.append(line);
+                }
+                System.out.println(JSON.parseObject(builder.toString()).toJSONString());
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public void setIn(File in) {
         this.in = in;
-    }
-
-    public String getConfigAsMap() {
-        try {
-            FileReader reader = new FileReader(in);
-            BufferedReader br = new BufferedReader (reader);
-
-            StringBuilder builder = new StringBuilder();
-            String line = "";
-            while ((line = br.readLine()) != null) {
-                builder.append(line);
-            }
-            System.out.println(JSON.parseObject(builder.toString()).toJSONString());
-            return builder.toString();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return "error )))>>>>>";
     }
 }
